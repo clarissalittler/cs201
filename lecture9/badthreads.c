01d19cf49964efe3d03d3ee1bcc7f77335660ab5
left_adjoint <left_adjoint@inconsistentuniverse.space>
2024-10-22 16:32:39 -0700

    basic semaphores

--- lecture9/badthreads.c
+++ lecture9/badthreads.c
@@ -5,28 +5,19 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <semaphore.h>
 
 int ourCounter = 0;
 
-sem_t ourSem;
-
-
-
 void* threadCounter(void* arg){
-  sem_wait(&ourSem);
   int temp = ourCounter;
   sleep(rand()%3);
   ourCounter = temp+1;
-  sem_post(&ourSem);
   return NULL;
 }
 
 int main(){
   srand(time(0));
   pthread_t threads[10];
-
-  sem_init(&ourSem,0,1);
 
   for(int i = 0;i < 10; i++){
     pthread_create(&threads[i],NULL,threadCounter,NULL);
@@ -36,8 +27,6 @@ int main(){
     pthread_join(threads[i],NULL);
   }
 
-  sem_destroy(&ourSem);
-  
   printf("What's the value of this counter?? %d\n",ourCounter);
   return 0;
 }
